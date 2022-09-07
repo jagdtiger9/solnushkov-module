a8f45c1999459c52cf238e548230ef8eab00de5a
cucaracha <cucaracha@mail.ru>
2022-09-07 17:30:27 +0300

    ..

--- Domain/Event/SystemUserCreated.php
+++ Domain/Event/SystemUserCreated.php
@@ -7,18 +7,10 @@ use Aljerom\Solnushkov\Domain\Entity\TemporaryUser;
 
 class SystemUserCreated implements Dto
 {
-    /**
-     * @var string
-     */
-    private $email;
+    public string $email;
 
     public function __construct(TemporaryUser $tempUser)
     {
         $this->email = $tempUser->email();
-    }
-
-    public function email(): string
-    {
-        return $this->email;
     }
 }
